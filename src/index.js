2ed1f7abbccf68590b42021dd6e91f5be9a5039e
Mayra Mosqueira <mosqueira.mayra@gmail.com>
2022-07-14 18:27:32 -0300

    agregué fetch

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 
+
 function Product(props) {
-    const altText = `Imagen de un ${props.value.tittle}`;
+    const altText = `Imagen de un ${props.value.title}`;
     return (
-        <article class = 'articulo'>
-            <img src={props.value.image} alt={altText}/>
+        <article className='articulo'>
+            <img src={props.value.image} alt={altText} />
             <h3> {props.value.title} </h3>
             <h3> $ {props.value.price} </h3>
         </article>
@@ -14,17 +15,27 @@ function Product(props) {
 }
 
 function ProductList(props) {
-    const products = [
-        { id: 1, image: "images/articulos/buzo1.jpg", title: "Buzo", price: 7500  },
-        { id: 2, image: "images/articulos/camisa1.jpg", title: 'Camisa', price: 6000 },
-        { id: 3, image: "images/articulos/gorro1.jpg", title: 'Gorro', price: 1500 },
-        { id: 4, image: "images/articulos/gorro2.jpg", title: 'Gorro', price: 1500 },
-        { id: 6, image: "images/articulos/pantalon1.jpg", title: 'Pantalon', price: 1200 },
-      ];
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const url = 'http://localhost:3000/main-products';
+
+        fetch(url)
+            .then(response => response.json())
+            .then((productos) => {
+                setProducts(productos)
+            })
+
+            .catch(err => console.log(err))
+
+    }, [])
+
+
 
     const productElements = products.map(product => (
-        <Product value={product}/>
-      ))
+        <Product key={product.id} value={product} />
+    ))
 
     return (
         <div className='articulos'>
@@ -35,3 +46,5 @@ function ProductList(props) {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<ProductList />);
+
+
